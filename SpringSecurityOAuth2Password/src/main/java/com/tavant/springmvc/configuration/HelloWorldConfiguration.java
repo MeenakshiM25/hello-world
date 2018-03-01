1b605af97f3383361d61014443696798cd978ffe
MeenakshiM25 <mail.m.meenakshi@gmail.com>
2018-03-01 20:04:43 +0530

    Add files via upload

---
+++ SpringSecurityOAuth2Password/src/main/java/com/tavant/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.tavant.springmvc.configuration;
+
+//import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = "com.tavant.springmvc")
+public class HelloWorldConfiguration {
+	
+
+}
---
+++ SpringSecurityOAuth2Password/src/main/java/com/tavant/springmvc/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.tavant.springmvc.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+// this class is mainly for registering delegating filter proxy with our war.hereOauth2SecurityConfiguration is the class that will be registered with initializer
+/*public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
+*/
